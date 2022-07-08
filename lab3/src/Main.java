aa218585ba27fdaacbc226195947bede6b3caaf5
Jesús David Cuéllar <jedave.co@gmail.com>
2022-07-07 21:53:17 -0500

    Inicio Lab3

--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,5 +1,74 @@
+import java.util.Arrays;
 public class Main {
+    public static void merge(Integer[] mainArray , int low, int middle, int high){
+        //Sizes temporal arrays
+        int length1 = middle - low + 1;
+        int length2 = high - middle;
+        Integer[] arrayLeft = new Integer[length1];
+        Integer[] arrayRight = new Integer[length2];
+        //copy temporal arrays
+
+        System.arraycopy(mainArray, low , arrayLeft, 0, length1);
+        System.arraycopy(mainArray, middle + 1, arrayRight, 0, length2);
+
+        int i = 0;
+        int j = 0;
+        int k = low;
+
+        while(i < length1 &&  j < length2){
+            if (arrayLeft[i] <= arrayRight[j]){
+                mainArray[k] = arrayLeft[i];
+                i = i + 1;
+            }else{
+                mainArray[k] = arrayRight[j];
+                j = j +1;
+            }
+            k = k+1;
+        }
+        while (i < arrayLeft.length){
+            mainArray[k] = arrayLeft[i];
+            i= i + 1;
+            k=k+1;
+        }
+        while (j < arrayRight.length){
+            mainArray[k] = arrayRight[j];
+            j= j + 1;
+            k=k + 1;
+        }
+
+    }
+    private static void mergeSort(Integer[] mainArray , int low, int high){
+        if (low < high){
+            int middle = low + (high - low) / 2;
+
+            mergeSort(mainArray, low, middle);
+            mergeSort(mainArray, middle+1, high);
+            merge(mainArray, low, middle, high);
+
+        }
+    }
+    public static void InsertionSort(Integer[] array){
+        for (int i=0; i < array.length ; i++ ){
+            int temp = array[i];
+
+            int j = i-1;
+
+            for(int m = j; m>=0 && array[m] > temp; m-- ){
+                array[m+1] = array[m];
+            }
+            array[j+1] = temp;
+
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] A = { 12, 14, 13, 5, 6, 7 };
+        Integer[] B = { 12, 14, 13, 5, 6, 7 };
+
+
+        mergeSort(A,0,A.length-1);
+        InsertionSort(B);
+
+        System.out.println(Arrays.asList(A));
+        System.out.println(Arrays.asList(B));
     }
 }
